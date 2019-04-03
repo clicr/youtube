2a3bf3b3007aadf054eb2f86139cbec5ec388c55
Tammy Huang <t.t.huang@hotmail.com>
2019-04-03 17:24:24 -0400

    Initial Commit

---
+++ youtube/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  youtube
+//
+//  Created by Tammy Huang on 2/8/19.
+//  Copyright © 2019 Charles Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = UIColor.red
+    }
+
+
+}
+
